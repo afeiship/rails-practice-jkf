9d14f291e53bf15fc57fa16b83078e7da39eac74
afeiship <1290657123@qq.com>
2018-02-09 10:43:58 +0800

    issue: format_time/date to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
     
     def format_time(time)
-        time.strftime("%Y-%m-%d %H:%M")
+        time.strftime("%Y-%m-%d %H:%M:%S")
     end
 
     def format_date(time)
-        time.strftime("%Y.%m.%d")
+        time.strftime("%Y-%m-%d")
+    end
+
+    def current_user
+        session[:current]
     end
     
 end
